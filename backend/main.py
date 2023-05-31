07738dffb7bf3d5c7bfef24a1927adfb4fc57b00
minato-legend <tigerrabbit22@outlook.com>
2023-05-31 08:30:39 -0700

    directory fixed

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,7 @@ def start_application():
     app = FastAPI(title = settings.APP_TITLE, version = settings.APP_VERSION)
     include_router(app)
     return app
-if __name__ == "__main__":
-    app = start_application()
-    uvicorn.run(app, host = settings.APP_HOST, port = int(settings.APP_PORT))
+app = start_application()
+# if __name__ == "__main__":
+#     app = start_application()
+#     uvicorn.run(app, host = settings.APP_HOST, port = int(settings.APP_PORT))
